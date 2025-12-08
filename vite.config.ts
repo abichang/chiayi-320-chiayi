4d12682b254c8108603659c479ba195a2638e572
Abi Chang <abichangtw@gmail.com>
2025-12-08 15:06:13 +0800

    fix: correct asset paths by setting base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/chiayi-320-chiayi/',
       server: {
         port: 3000,
         host: '0.0.0.0',
